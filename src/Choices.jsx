4aad2a5e77472022b2f5a742fb64a17215b12fd0
Siyabonga Mahlalela <123079710+vusiSiya@users.noreply.github.com>
2023-06-02 15:34:02 +0200

    Update Choices.jsx

--- src/Choices.jsx
+++ src/Choices.jsx
@@ -3,25 +3,72 @@ import React from "react"
 export default [
   {
     id: 1,
-    title: "Fashion",
+    title: "",
     img: './src/Images/Men/EmaSwati/swati-man-in-traditional-attire.jpg',
-    name: "Lihiya",
-    price: 9
-
+    name: "Siswati Attire",
+    price: 60
   },
   {
     id: 2,
-    title: "Watches",
-    img: "./src/Images/Watches/Heuer-SilverSnoe.jpg",
-    name: "Heuer Silver Snow watch",
-    price: 9
+    title: "",
+    img: "src/Images/Men/BaSotho/sotho-man-in-sotho-blanket.png",
+    name: "Sotho Blanket",
+    price: 1200
   },
   {
     id: 3,
-    title: "Computers",
-    img: "./src/Images/Computers/All-in-one-2-sin-fondo.png",
+    title: "",
+    img: "src/Images/Men/AmaXhosa/xhosa-man-in-white.png",
+    name: "Xhosa Attire",
+    price: 15000
+  },
+  {
+    id: 4,
+    title: "",
+    img: "src/Images/Men/AmaXhosa/xhosa-guy-in-white.jpg",
+    name: "Xhosa Attire",
+    price: 15000
+  },
+  {
+    id: 5,
+    title: "",
+    img: "src/Images/Men/AmaXhosa/the-xhosa-man-in-blue-and-white.png",
+    name: "Xhosa Attire",
+    price: 15000
+  },
+  {
+    id: 6,
+    title: "",
+    img: "src/Images/Men/EmaSwati/swati-man-at-mountain.png",
+    name: "Swati Attire",
+    price: 15000
+  },
+  {
+    id: 7,
+    title: "",
+    img: "src/Images/Men/BaSotho/sotho-guy-at-entrande.png",
+    name: "Morden Sotho Attire",
+    price: 15000
+  },
+  {
+    id: 8,
+    title: "",
+    img: "src/Images/Women/Basotho/woman-blanket-jacket.png",
+    name: "",
+    price: 15000
+  },
+  {
+    id: 9,
+    title: "",
+    img: "src/Images/Women/EmaSwati/swati-woman-in-blue-dress.png",
+    name: "",
+    price: 15000
+  },
+  {
+    id: 10,
+    title: "",
+    img: "src/Images/Women/Basotho/sotho-woman-in-seshweshwe.png",
     name: "hp 400 G6 All in One PC",
     price: 15000
   }
-
 ]
