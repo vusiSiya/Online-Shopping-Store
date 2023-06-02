454bf166c7cb18f92bc95e56b87913bd572c4cb1
Siyabonga Mahlalela <123079710+vusiSiya@users.noreply.github.com>
2023-06-02 15:32:16 +0200

    Update Items.jsx

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 
+
 export default function Items(props) {
-  //const { product } = props;
-  //console.log(product)
+
   return (
     <div className='product'>
       <h2 className="product--title" >
         {props.product.title}
       </h2>
-      <img src={`${props.product.img}`} className='product--img' />
-      <p className='product--name'>
-        {props.product.name}
-      </p>
-      <p className='price'>
-        Price: R{props.product.price}
-      </p>
+      <div className="image-wrapper">
+           <img src={`${props.product.img}`} className='product--img' />
+      </div>
+      <div>
+        <p className='product--name'>
+          {props.product.name}
+        </p>
+        <p className='price'>
+          R{props.product.price}
+        </p>
+        <button onClick={props.handleClick} className='Btn--cart'>Add to Cart</button>
+      </div>
     </div>
-
   )
 }
