e8e0ccadd2646574f7502a4ccf4bc3a7a37348b9
Siyabonga Mahlalela <siyabonga.mahlalela@imaginescholar.org>
2023-06-17 01:23:58 +0200

    updating project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,79 @@
+import React from 'react'
 import './App.css'
 import NavBar from './Components/NavBar'
 import Items from './Components/Items'
 import Footer from './Components/Footer'
-import choices from './Choices'
+import data from './Data'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 export default function App() {
-  const products = choices.map((item) => {
-    return <Items
+  const [count, setCount] = React.useState(0)
+
+  function handleClick(id) {
+    setCount(prevCount => prevCount + 1)
+  }
+  return (
+    <BrowserRouter>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/men" element={<Men />} />
+
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+function Home() {
+  let products = []
+  //const getItems = ()=>{}
+  for (let i = 1; i <= 8; i++) {
+    let randomNum = Math.floor(Math.random() * 3) + 1
+    let selection = randomNum === 1 ?
+      data.girls
+      :
+      randomNum === 2 ? data.watches : data.guys;
+
+    let randomImage = Math.floor(Math.random() * 6) + 1
+    let item = randomNum === 2 ?
+      data.watches[0] : selection[randomImage];
+
+    products[i] = <Items
+      key={item.id}
       product={item}
+      handleClick={item => handleClick(item.id)}
+    />
+  }
+  return (
+    <>
+      <h3 className='slogan'>Choose your best look</h3>
+      <div className="products--parent">
+        {products}
+      </div>
+    </>
+  )
+}
+
+function Men() {
+  const firstArray = data.guys.map( item => console.log(item.price))
+  const clothes = data.guys.map(item => {
+    <Items
+      key={item.id}
+      product={item}
+      handleClick={item => handleClick(item.id)}
     />
   })
-  console.log(products)
   return (
-    <main>
-      <NavBar />
-      <h3 className='slogan'>Look the part</h3>
-      <ul className="products--parent">{products}</ul>
-      <Footer />
-    </main>
+    <>
+      <h3 className='slogan'>Men</h3>
+      <div className="products--parent">
+        {clothes}
+      </div>
+    </>
   )
 }
